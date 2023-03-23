a846a591a334fb1077b260a6eae8c20b4036232a
Edinorig <super-ddred@ukr.net>
2023-03-23 11:47:50 +0100

    test

---
+++ src/helpers/helper.js
@@ -0,0 +1,27 @@
+const loginInput = {
+    name: '',
+    list:[
+        {
+            title: 'Login',
+            inputType: 'text',
+            value: '',
+            maxLenght: '',
+            required: 'required',
+            className: 'input-data',
+            placeHolder: 'login',
+            validate: '',
+        },
+        {
+            title: 'Pass',
+            inputType: 'pass',
+            value: '',
+            maxLenght: '',
+            required: 'required',
+            className: 'input-data',
+            placeHolder: 'pass',
+            validate: '',
+        }
+    ]
+}
+
+export {loginInput}
---
+++ src/lib/log-wrapper/log-wrapper.js
@@ -0,0 +1,67 @@
+import Field from "../filed-wrapper/field-wrapper.js";
+
+class LogWrapper {
+    constructor(parent, props) {
+        this.parentElement = parent;
+        this.props = props;
+
+        this.template;
+        
+        this.listFileds = [];
+    }
+
+    init() {
+        this.initElements();
+        this.initEventListeners();
+        this.initField();
+    }
+
+    destroy() {
+    }
+
+    initElements() {
+        this.template = this.initTemplate();
+
+        this.elements = {
+            inputComponent: this.template.querySelector('.input-wrapper'),
+        }
+        
+
+        this.parentElement.appendChild(this.template);
+    }
+
+    initEventListeners() {
+    }
+
+    initTemplate() {
+        const parser = new DOMParser();
+        const templateString =`
+        <div class="main-content">
+            <div class="logo">
+                <h1>Login</h1>
+            </div>
+            <div class="input-wrapper">
+
+            </div>
+            <button class="submit-btn"><h3>Submit</h3></button>
+        </div>`;
+        const templateElement = parser.parseFromString(templateString, 'text/html');
+        return templateElement.documentElement.querySelector("body > div");
+    }
+
+    initField(){
+        console.log(this.parentElement);
+        console.log(this.template);
+        console.log(this.elements.inputComponent);
+        this.props.forEach(props => {
+            const fieldWrapper = new Field (this.elements.inputComponent, props)
+            console.log(props);
+            fieldWrapper.init();
+            const el = fieldWrapper.render();
+            this.elements.inputComponent.appendChild(el);
+            this.listFileds.push(fieldWrapper);
+        }); 
+    }
+}
+
+export default LogWrapper;
